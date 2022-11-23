d0ac1979ad01493375b2696bc85909021fe67cd6
George Barrett <georgeaustinbarrett@gmail.com>
2022-11-23 18:10:07 +0000

    initial commit for more_practice_pt2

---
+++ more_practice_pt2.py
@@ -0,0 +1,17 @@
+import more_practice
+
+sentence = 'All good things come to those who wait.'
+words = more_practice.break_words(sentence)
+words
+sorted_words = more_practice.sort_words(words)
+sorted_words
+more_practice.print_first_word(words)
+more_practice.print_last_word(words)
+words
+more_practice.print_first_word(sorted_words)
+more_practice.print_last_word(sorted_words)
+sorted_words
+sorted_words = more_practice.sort_sentence(sentence)
+sorted_words
+more_practice.print_first_and_last(sentence)
+more_practice.print_first_and_last_sentence_sorted(sentence)
